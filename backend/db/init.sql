1008876316171a7e64774fae837872df13ee8029
Sifat Al Islam <sifat.a.5532@gmail.com>
2026-08-06 22:08:14 +0600

    Format init.sql file

--- backend/db/init.sql
+++ backend/db/init.sql
@@ -54,9 +54,9 @@ CREATE TABLE if NOT EXISTS projects (
    CONSTRAINT fk_project_author FOREIGN KEY (author_id) REFERENCES users (id) ON DELETE CASCADE
 );
 
-ALTER TABLE projects ADD CONSTRAINT  fk_project_cloned_from FOREIGN key(cloned_from_id) REFERENCES projects(id) ON DELETE SET NULL;
+ALTER TABLE projects ADD CONSTRAINT fk_project_cloned_from FOREIGN key (cloned_from_id) REFERENCES projects (id) ON DELETE SET NULL;
 
-ALTER TABLE projects ADD CONSTRAINT fk_template_originates_from FOREIGN key(originates_from_id) REFERENCES projects(id) ON DELETE SET NULL;
+ALTER TABLE projects ADD CONSTRAINT fk_template_originates_from FOREIGN key (originates_from_id) REFERENCES projects (id) ON DELETE SET NULL;
 
 CREATE TABLE if NOT EXISTS project_cors_origin (
    project_id INTEGER NOT NULL,
@@ -95,83 +95,73 @@ CREATE TABLE if NOT EXISTS project_logs (
    entity_type VARCHAR(30) NOT NULL,
    entity_id INTEGER NOT NULL,
    change_type VARCHAR(20) NOT NULL,
-   old_data JSON ,
+   old_data JSON,
    new_data JSON,
    CONSTRAINT fk_project_logs_project FOREIGN KEY (project_id) REFERENCES projects (id) ON DELETE CASCADE,
    CONSTRAINT fk_project_logs_changed_by FOREIGN Key (changed_by) REFERENCES users (id) ON DELETE SET NULL
-       
-  
 );
+
 -- indexes on (project id,changed_at), (entity_type, entity_id)
-CREATE INDEX if NOT EXISTS idx_project_logs_project_id_created_at ON project_logs (project_id,created_at);
+CREATE INDEX if NOT EXISTS idx_project_logs_project_id_created_at ON project_logs (project_id, created_at);
 
-CREATE INDEX if NOT EXISTS idx_project_logs_entity_type_entity_id ON project_logs (entity_type,entity_id,created_at DESC);
-
+CREATE INDEX if NOT EXISTS idx_project_logs_entity_type_entity_id ON project_logs (entity_type, entity_id, created_at DESC);
 
 CREATE TABLE if NOT EXISTS project_collaborators (
    project_id INTEGER NOT NULL,
    user_id INTEGER NOT NULL,
    created_at TIMESTAMP NOT NULL DEFAULT now (),
-   role VARCHAR(15)  NOT NULL ,
+   role VARCHAR(15) NOT NULL,
    status VARCHAR(15) NOT NULL DEFAULT 'pending',
    CONSTRAINT fk_collaborates_project FOREIGN KEY (project_id) REFERENCES projects (id) ON DELETE CASCADE,
    CONSTRAINT fk_collaborates_user_id FOREIGN Key (user_id) REFERENCES users (id) ON DELETE CASCADE,
-   PRIMARY KEY (project_id, user_id) 
+   PRIMARY KEY (project_id, user_id)
 );
 
 CREATE TABLE if NOT EXISTS schema_tables (
    id serial PRIMARY KEY,
    project_id INTEGER NOT NULL,
    table_name VARCHAR(30) NOT NULL,
-    db_schema_name VARCHAR(30) NOT NULL,
+   db_schema_name VARCHAR(30) NOT NULL,
    created_at TIMESTAMP NOT NULL DEFAULT now (),
    CONSTRAINT fk_schema_tables_project_id FOREIGN Key (project_id) REFERENCES projects (id) ON DELETE CASCADE
-
 );
 
-CREATE INDEX if NOT EXISTS idx_schema_tables_project_id_table_name ON schema_tables (project_id,table_name);
-
+CREATE INDEX if NOT EXISTS idx_schema_tables_project_id_table_name ON schema_tables (project_id, table_name);
 
 CREATE TABLE if NOT EXISTS schema_columns (
    id serial PRIMARY KEY,
    schema_table_id INTEGER NOT NULL,
    col_name VARCHAR(30) NOT NULL,
    col_type VARCHAR(20) NOT NULL,
-   default_value VARCHAR(30) ,
+   default_value VARCHAR(30),
    col_length INTEGER,
    is_primary_key BOOLEAN DEFAULT FALSE,
-      is_auto_increment BOOLEAN DEFAULT FALSE,
-     is_nullable BOOLEAN DEFAULT TRUE,
-     is_unique BOOLEAN DEFAULT FALSE,
-      created_at TIMESTAMP NOT NULL DEFAULT now (),
-
-   CONSTRAINT fk_schema_columns_schema_table_id FOREIGN Key(schema_table_id) REFERENCES schema_tables(id) ON DELETE CASCADE
-
+   is_auto_increment BOOLEAN DEFAULT FALSE,
+   is_nullable BOOLEAN DEFAULT TRUE,
+   is_unique BOOLEAN DEFAULT FALSE,
+   created_at TIMESTAMP NOT NULL DEFAULT now (),
+   CONSTRAINT fk_schema_columns_schema_table_id FOREIGN Key (schema_table_id) REFERENCES schema_tables (id) ON DELETE CASCADE
 );
 
-CREATE INDEX if NOT EXISTS idx_schema_columns_table_id_col_name ON schema_columns (schema_table_id,col_name);
+CREATE INDEX if NOT EXISTS idx_schema_columns_table_id_col_name ON schema_columns (schema_table_id, col_name);
 
 CREATE TABLE if NOT EXISTS schema_foreign_keys (
-
    child_col_id INTEGER PRIMARY KEY,
    parent_col_id INTEGER NOT NULL,
    fk_name VARCHAR(30) NOT NULL,
    db_schema_name VARCHAR(30) NOT NULL,
-   on_delete VARCHAR(20) ,
+   on_delete VARCHAR(20),
    on_update VARCHAR(20),
    created_at TIMESTAMP NOT NULL DEFAULT now (),
-
    CONSTRAINT fk_schema_fks_child FOREIGN Key (child_col_id) REFERENCES schema_columns (id) ON DELETE CASCADE,
    CONSTRAINT fk_schema_fks_parent FOREIGN Key (parent_col_id) REFERENCES schema_columns (id) ON DELETE CASCADE
 );
-
-
 
 CREATE TABLE if NOT EXISTS api_definitions (
    id serial PRIMARY KEY,
    name VARCHAR(30) NOT NULL,
    project_id INTEGER NOT NULL,
-   method VARCHAR(15) NOT NULL,
+   METHOD VARCHAR(15) NOT NULL,
    query_definition JSON NOT NULL,
    generated_sql TEXT NOT NULL,
    parameters TEXT,
@@ -180,83 +170,80 @@ CREATE TABLE if NOT EXISTS api_definitions (
    updating_parameters TEXT,
    created_at TIMESTAMP NOT NULL DEFAULT now (),
    CONSTRAINT fk_api_definitions_project_id FOREIGN Key (project_id) REFERENCES projects (id) ON DELETE CASCADE,
-   CONSTRAINT unique_api_definitions_project_id_name UNIQUE(project_id,name)
-   
-
+   CONSTRAINT unique_api_definitions_project_id_name UNIQUE (project_id, name)
 );
 
-CREATE INDEX if NOT EXISTS idx_api_definitions ON api_definitions (project_id,name);
+CREATE INDEX if NOT EXISTS idx_api_definitions ON api_definitions (project_id, name);
 
 CREATE TABLE if NOT EXISTS api_logs (
    id serial PRIMARY key,
-   api_definition_id INTEGER NOT NULL ,
+   api_definition_id INTEGER NOT NULL,
    ip_address INET NOT NULL,
    status_code INTEGER NOT NULL,
    response_time_ms INTEGER NOT NULL,
    created_at TIMESTAMP NOT NULL DEFAULT now (),
-   CONSTRAINT fk_api_logs_api_definitions FOREIGN Key (api_definition_id) REFERENCES api_definitions (id) ON DELETE  CASCADE
-
+   CONSTRAINT fk_api_logs_api_definitions FOREIGN Key (api_definition_id) REFERENCES api_definitions (id) ON DELETE CASCADE
 );
-
 
 CREATE TABLE if NOT EXISTS api_table_dependencies (
    api_definition_id INTEGER NOT NULL,
-   schema_table_id INTEGER NOT NULL ,
+   schema_table_id INTEGER NOT NULL,
    usage_context VARCHAR(30),
    created_at TIMESTAMP NOT NULL DEFAULT now (),
-   PRIMARY KEY(api_definition_id ,schema_table_id),
+   PRIMARY KEY (api_definition_id, schema_table_id),
    CONSTRAINT fk_api_table_dependencies_api_definition_id FOREIGN Key (api_definition_id) REFERENCES api_definitions (id) ON DELETE CASCADE,
    CONSTRAINT fk_api_table_dependencies_api_schema_table_id FOREIGN Key (schema_table_id) REFERENCES schema_tables (id) ON DELETE RESTRICT
 );
 
-
 CREATE TABLE if NOT EXISTS api_column_dependencies (
    api_definition_id INTEGER NOT NULL,
-   schema_col_id INTEGER NOT NULL ,
+   schema_col_id INTEGER NOT NULL,
    usage_context VARCHAR(30),
    created_at TIMESTAMP NOT NULL DEFAULT now (),
-   PRIMARY KEY(api_definition_id ,schema_col_id),
+   PRIMARY KEY (api_definition_id, schema_col_id),
    CONSTRAINT fk_api_column_dependencies_api_definition_id FOREIGN Key (api_definition_id) REFERENCES api_definitions (id) ON DELETE CASCADE,
    CONSTRAINT fk_api_column_dependencies_schema_col_id FOREIGN Key (schema_col_id) REFERENCES schema_columns (id) ON DELETE RESTRICT
 );
-CREATE TABLE if NOT EXISTS template_clones(
-    id serial PRIMARY KEY,
+
+CREATE TABLE if NOT EXISTS template_clones (
+   id serial PRIMARY KEY,
    user_id INTEGER NOT NULL,
-   template_id INTEGER NOT NULL ,
-   cloned_project_id INTEGER ,
+   template_id INTEGER NOT NULL,
+   cloned_project_id INTEGER,
    created_at TIMESTAMP NOT NULL DEFAULT now (),
    CONSTRAINT fk_template_clones_user_id FOREIGN Key (user_id) REFERENCES users (id) ON DELETE CASCADE,
-   CONSTRAINT fk_template_clones_template_id FOREIGN Key ( template_id) REFERENCES projects(id) ON DELETE CASCADE
+   CONSTRAINT fk_template_clones_template_id FOREIGN Key (template_id) REFERENCES projects (id) ON DELETE CASCADE
 );
-CREATE TABLE if NOT EXISTS template_feedback(
-    id serial PRIMARY KEY,
+
+CREATE TABLE if NOT EXISTS template_feedback (
+   id serial PRIMARY KEY,
    user_id INTEGER NOT NULL,
-   template_id INTEGER NOT NULL ,
-   message TEXT NOT NULL ,
+   template_id INTEGER NOT NULL,
+   message TEXT NOT NULL,
    is_read BOOLEAN DEFAULT FALSE,
    created_at TIMESTAMP NOT NULL DEFAULT now (),
    CONSTRAINT fk_template_feedback_user_id FOREIGN Key (user_id) REFERENCES users (id) ON DELETE CASCADE,
-   CONSTRAINT fk_template_feedback_template_id FOREIGN Key ( template_id) REFERENCES projects(id) ON DELETE CASCADE
+   CONSTRAINT fk_template_feedback_template_id FOREIGN Key (template_id) REFERENCES projects (id) ON DELETE CASCADE
 );
 
-CREATE INDEX if NOT EXISTS idx_feedback_user_id_template_id ON template_feedback (user_id,template_id);
+CREATE INDEX if NOT EXISTS idx_feedback_user_id_template_id ON template_feedback (user_id, template_id);
 
-CREATE TABLE if NOT EXISTS template_ratings(
+CREATE TABLE if NOT EXISTS template_ratings (
    user_id INTEGER NOT NULL,
-   template_id INTEGER NOT NULL ,
-   rating INTEGER CONSTRAINT rating_range_check CHECK(rating BETWEEN 1 AND 5),
-   review_text TEXT ,
+   template_id INTEGER NOT NULL,
+   rating INTEGER CONSTRAINT rating_range_check CHECK (rating BETWEEN 1 AND 5),
+   review_text TEXT,
    created_at TIMESTAMP NOT NULL DEFAULT now (),
-   PRIMARY KEY(user_id,template_id),
+   PRIMARY KEY (user_id, template_id),
    CONSTRAINT fk_template_ratings_user_id FOREIGN Key (user_id) REFERENCES users (id) ON DELETE CASCADE,
-   CONSTRAINT fk_template_ratings_template_id FOREIGN Key ( template_id) REFERENCES projects(id) ON DELETE CASCADE
+   CONSTRAINT fk_template_ratings_template_id FOREIGN Key (template_id) REFERENCES projects (id) ON DELETE CASCADE
 );
 
-CREATE TABLE if NOT EXISTS template_likes(
+CREATE TABLE if NOT EXISTS template_likes (
    user_id INTEGER NOT NULL,
-   template_id INTEGER NOT NULL ,
+   template_id INTEGER NOT NULL,
    created_at TIMESTAMP NOT NULL DEFAULT now (),
-    PRIMARY KEY(user_id,template_id),
+   PRIMARY KEY (user_id, template_id),
    CONSTRAINT fk_template_likes_user_id FOREIGN Key (user_id) REFERENCES users (id) ON DELETE CASCADE,
-   CONSTRAINT fk_template_likes_template_id FOREIGN Key ( template_id) REFERENCES projects(id) ON DELETE CASCADE
+   CONSTRAINT fk_template_likes_template_id FOREIGN Key (template_id) REFERENCES projects (id) ON DELETE CASCADE
 );
