97dd932139bf7ef425cd44bf08dc35fb921b25e3
ZTIshra19 <ishrazarrintasnim@gmail.com>
2026-08-09 02:54:47 +0600

    Fix regenerate apikey

--- backend/routes/project.js
+++ backend/routes/project.js
@@ -4,9 +4,9 @@ const query = require('./../db/query');
 const router = express.Router();
 const { requireAuth } = require('./auth');
 
-router.post('/createProject',requireAuth, async (req, res) => {
-    const { proj_name,description, enable_auth, tags } = req.body;
-    const author_id =  req.loggedInUser.id;
+router.post('/createProject', requireAuth, async (req, res) => {
+    const { proj_name, description, enable_auth, tags } = req.body;
+    const author_id = req.loggedInUser.id;
     if (!proj_name) {
         return res.status(400).json({ msg: 'Please fill in project name' });
 
@@ -62,22 +62,20 @@ router.post('/createProject',requireAuth, async (req, res) => {
     res.status(201).json({ msg: 'Project created successfully', api_key: api_key });
 }
 )
-router.post('/regenApiKey',requireAuth,async(req,res)=>{
-    const user_id=req.loggedInUser.id;
-    const {proj_name}=req.body;
-     const api_key = crypto.randomBytes(32).toString('hex');
+router.post('/regenerateKey', requireAuth, async (req, res) => {
+    const user_id = req.loggedInUser.id;
+    const { proj_id } = req.body;
+    const api_key = crypto.randomBytes(32).toString('hex');
     const api_key_prefix = api_key.substring(0, 6);
     const api_key_hashed = crypto.createHash('sha256').update(api_key).digest('hex');
-    const result=await query('SELECT id FROM projects WHERE name=$1 AND author_id=$2',[proj_name,user_id]);
-  
-
-   
-    if(result.rows.length===0){
-             return res.status(400).json({msg:"You don't have access to change the api key"});
+    const result = await query('SELECT * FROM projects WHERE id=$1 AND author_id=$2', [proj_id, user_id]);
+    if (result.rows.length === 0) {
+        return res.status(400).json({ msg: "You don't have access to change the api key" });
     }
-    else {   const proj_id=result.rows[0].id;
-        await query('UPDATE projects SET api_key_hashed=$1 , api_key_prefix=$2 WHERE id=$3',[api_key_hashed,api_key_prefix,proj_id]);
-    res.status(200).json({msg:'Api key regenerated successfully',api_key:api_key});}
+    else {
+        await query('UPDATE projects SET api_key_hashed=$1 , api_key_prefix=$2 WHERE id=$3', [api_key_hashed, api_key_prefix, proj_id]);
+        res.status(200).json({ msg: 'Api key regenerated successfully', api_key: api_key });
+    }
 })
 module.exports = router;
 
