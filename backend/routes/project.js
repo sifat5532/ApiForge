f4fb7942f9572195ec73cb6aeed06c4dc2f6e34e
ZTIshra19 <ishrazarrintasnim@gmail.com>
2026-08-09 15:35:20 +0600

    Add feedback template and fix rating twmplate

--- backend/routes/project.js
+++ backend/routes/project.js
@@ -125,7 +125,7 @@ router.post('/proceedCollabInvitation', requireAuth, async (req, res) => {
 });
 router.post('/likeTemplate', requireAuth, async (req, res) => {
     const { template_id, isLike } = req.body;
-    const isExist = await query('SELECT * FROM projects WHERE id=$1 AND is_template=$2', [template_id, false]);
+    const isExist = await query('SELECT * FROM projects WHERE id=$1 AND is_template=$2', [template_id, true]);
     if (isExist.rows.length === 0) {
         return res.status(404).json({ msg: "Template not found" });
     }
@@ -139,13 +139,13 @@ router.post('/likeTemplate', requireAuth, async (req, res) => {
     }
     if (!isLike) return res.status(400).json({ msg: "You have not yet like the template" });
     await query('INSERT INTO template_likes(template_id,user_id) VALUES($1,$2)', [template_id, req.loggedInUser.id]);
-    res.status(200).json({ msg: "Successfully like the template" });
+    res.status(200).json({ msg: "Successfully liked the template" });
 });
 router.post('/rateTemplate', requireAuth, async (req, res) => {
     const { template_id, rating, review } = req.body;
-    const isExist = await query('SELECT * FROM projects WHERE id=$1 AND is_template=$2', [template_id, false]);
+    const isExist = await query('SELECT * FROM template_clones WHERE id=$1 AND template_id=$2', [req.loggedInUser.id,template_id]);
     if (isExist.rows.length === 0) {
-        return res.status(404).json({ msg: "Template not found" });
+        return res.status(400).json({ msg:"You have to clone the template first to rate it" });
     }
     if (!(rating >= 1 && rating <= 5)) {
         return res.status(400).json({ msg: "Rating should be integer between 1 to 5" });
@@ -156,11 +156,27 @@ router.post('/rateTemplate', requireAuth, async (req, res) => {
     const ratingExist = await query('SELECT * FROM template_ratings WHERE template_id=$1 AND user_id=$2', [template_id, req.loggedInUser.id]);
     if (ratingExist.rows.length > 0) {
         if (rating) await query('UPDATE  template_ratings SET rating=$1 , review_text=$2,created_at=CURRENT_TIMESTAMP WHERE template_id=$3 AND user_id=$4', [rating, review, template_id, req.loggedInUser.id]);
-        return res.status(200).json({ msg: "Successfully update the rating" });
+        return res.status(200).json({ msg: "Successfully updated the rating" });
     }
     await query('INSERT INTO template_ratings(template_id,user_id,rating,review_text) VALUES($1,$2,$3,$4)', [template_id, req.loggedInUser.id, rating, review]);
-    res.status(200).json({ msg: "Successfully rate the template" });
-})
+    res.status(200).json({ msg: "Successfully rated the template" });
+});
+router.post('/feedbackTemplate', requireAuth, async (req, res) => {
+    const { template_id, message } = req.body;
+    const isExist = await query('SELECT * FROM template_clones WHERE id=$1 AND template_id=$2', [req.loggedInUser.id,template_id]);
+    // if (isExist.rows.length === 0) {
+    //     return res.status(400).json({ msg:"You have to clone the template first to give feedback" });
+    // }
+      if (message.trim().length==0) {
+        return res.status(400).json({ msg: "You can not send empty feedback" });
+    }
+    if (message.length > 500) {
+        return res.status(400).json({ msg: "Please give feedback within 500 characters" });
+    }
+    await query('INSERT INTO template_feedback(template_id,user_id,message) VALUES($1,$2,$3)', [template_id, req.loggedInUser.id, message]);
+    res.status(200).json({ msg: "Successfully gave feedback to the template" });
+});
+
 
 
 
